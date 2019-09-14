b36885ac0c044e4a9a30b4dc3697f82943dfeab6
Kevin Buchik <kbuchik@gmail.com>
2019-09-14 11:45:56 -0700

    reworking logic

--- src/App.js
+++ src/App.js
@@ -7,62 +7,66 @@ import friends from "./friends.json";
 class App extends React.Component {
   state = {
     friends,
-    score: 0,
+    score:0,
     highScore: 0
   };
 
+  
 
+  handleClick = id => {
+  let friends=this.state.friends
 
+   let score=this.state.score
+   let highScore=this.state.highScore
+   friends.sort(() => Math.random() - 0.5)
+   const clickedFriend = this.state.friends.filter(friend => friend.id === id);
+    clickedFriend[0].clicked=true
+    if (clickedFriend[0].clicked) {
+      clickedFriend[0].clicked = false;
 
-  handleClick = (id, clicked) => {
-    const friendsClick = this.state.friends;
+      this.setState({ score:this.state.score+1 });
+      this.setState({ highScore:this.state.highScore+1 });
 
-    if (clicked && this.state.score<friends.length) {
-      friendsClick.forEach((friend, i) => {
-        friendsClick[i].clicked = false;
-      });
-      return this.setState({
-        friend: friendsClick.sort(() => Math.random() - 0.5),
-
-        score: 0
-      })
-    } 
-    else {
-      friendsClick.forEach((friend, i) => {
-        if (id === friend.id) {
-          friendsClick[i].clicked = true;
-        }
-      });
-      const { highScore, score } = this.state;
-      const scored = score + 1;
-      const newHighScore = scored > highScore ? scored : highScore;
-
-      return this.setState({
-        friend: friendsClick.sort(() => Math.random() - 0.5),
-        score: scored,
-        highScore: newHighScore,
-      })
     }
-  };
+    else if (clickedFriend[0].clicked === false) {
+      this.setState({ score:score=0 });
 
+    }
+    if (score===8 || friends.clicked===false){
+    this.setState({ score:this.state.score=0 });
+    this.setState({ highScore:this.state.highScore=8 });
+
+    clickedFriend.clicked=false
+
+    }if (score>highScore){
+      this.setState({ friends });
+      this.setState({score})
+
+
+    }
+
+    return 
+
+    
+  };
 
 
   render() {
     return (
       <Wrapper>
-        <div className="scores">
+              <div className="scores">
           Score: {this.state.score}<br></br>
           High Score: {this.state.highScore}
         </div>
         <Title>Memory Game</Title>
+        
 
-
-        {this.state.friends.map(friend => <FriendCard handleClick={this.handleClick} key={friend.id} id={friend.id} image={friend.image} {...friends} />)}
+        {this.state.friends.map(friend => <FriendCard handleClick={this.handleClick} key={friend.id} id={friend.id} image={friend.image} {...friend} />)}
 
       </Wrapper>
     )
   }
-
 }
+
 
 export default App;
